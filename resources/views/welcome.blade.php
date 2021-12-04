5eb1e81617a4bd9ce8d36c0cdb6d67635d374c7f
martinezart87 <93553194+martinezart87@users.noreply.github.com>
2021-12-04 23:07:06 +0100

    zmiany w szablonie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Kursy walut - NBP</title>
-
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
-
 </head>
 
 <body>
@@ -21,44 +18,30 @@
         </div>
     </nav>
     <div class="container">
-        {{-- <button class="btn btn-warning my-2 my-sm-0" type="submit">Zaktualizuj</button> --}}
-        <a href="{{ url('/update/') }}" class="btn btn-warning my-2 my-sm-0">Zaktualizuj</a>
-
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody>
-        </table>
-
-            {{ $nbpapi }}
-
+        <div style="padding-top: 20px; padding-bottom: 20px">
+            <a href="{{ url('/update/') }}" class="btn btn-warning my-2 my-sm-0">Zaktualizuj</a>
+        </div>
+        <div class="row">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Nazwa waluty</th>
+                        <th scope="col">Kod</th>
+                        <th scope="col">Wartość kursu</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($nbpapi as $item)
+                        <tr>
+                            <th>{{ $item->name }} </th>
+                            <td>{{ $item->currency_code }}</td>
+                            <td>{{ $item->exchange_rate }} PLN</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-
 </body>
 
 </html>
